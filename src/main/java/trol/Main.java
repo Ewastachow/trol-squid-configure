b47791897cd0433da0a425281fcf2273389e9a09
Ewastachow <stachow.ewa@gmail.com>
2017-11-28 16:52:17 +0100

    v1.1 Black List skeleton

--- src/main/java/trol/Main.java
+++ src/main/java/trol/Main.java
@@ -4,4 +4,41 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Main run");
     }
+
+//    public static void main(String[] args){
+//        try {
+//            restartSquid();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//    }
+//
+//    private void askingLoop(){
+//
+//
+//    }
+//
+//    private static void restartSquid() throws IOException, InterruptedException {
+//        Process proc = Runtime.getRuntime().exec("service squid restart");
+//
+////        proc.waitFor();
+//        // Read the output
+//
+//        BufferedReader reader =
+//                new BufferedReader(new InputStreamReader(proc.getInputStream()));
+//
+//        String line = "";
+//        while((line = reader.readLine()) != null) {
+//            System.out.print(line + "\n");
+//        }
+//
+//        proc.waitFor();
+//
+//    }
+
+
 }
---
+++ src/main/java/trol/filter/domain_list/DomainList.java
@@ -0,0 +1,40 @@
+package trol.filter.domain_list;
+
+import com.sun.org.apache.xalan.internal.xsltc.DOM;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DomainList {
+
+    private List<String> domainList = new ArrayList<String>();
+
+    public String toString(){
+        //TODO: Implement
+        return null;
+    }
+
+    public DomainList fromString(String string) {
+        //TODO: Implement
+        return null;
+    }
+
+    public void toFile(String path){
+        //TODO: Implement
+    }
+
+    public DomainList fromFile(String path){
+        //TODO: Implement
+        return null;
+    }
+
+    public DomainList addDomain(String domain){
+        //TODO: Implement
+        return null;
+    }
+
+    public DomainList removeDomain(String domain){
+        //TODO: Implement
+        return null;
+    }
+}
