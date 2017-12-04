2fa3374770703df36b5a6463f1d5e805feb78f98
Ewastachow <stachow.ewa@gmail.com>
2017-12-04 21:18:32 +0100

    application.properties added

--- src/main/java/trol/controller/MainController.java
+++ src/main/java/trol/controller/MainController.java
@@ -1,10 +1,12 @@
 package trol.controller;
 
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 public class MainController {
     @RequestMapping("/")
-    String index() {
+    String index(ModelMap model) {
+        model.addAttribute("fullName", "Lama");
         return "index";
     }
 }
