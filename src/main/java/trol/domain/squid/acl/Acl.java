25811f0fc1e3c2f64326d1c6875a50fb636fbd41
Ewastachow <stachow.ewa@gmail.com>
2017-12-11 15:38:15 +0100

    TrolAPI Skeleton added

--- src/main/java/trol/domain/squid/acl/Acl.java
+++ src/main/java/trol/domain/squid/acl/Acl.java
@@ -3,6 +3,7 @@ package trol.domain.squid.acl;
 import trol.domain.squid.ConfigElem;
 
 public abstract class Acl implements ConfigElem{
+    boolean isCommented;
     String configString;
     AclType aclType;
 
---
+++ src/main/java/trol/domain/trol_api/header/TransmisionType.java
@@ -0,0 +1,7 @@
+package trol.domain.trol_api.header;
+
+public enum TransmisionType {
+    VIDEO, VIDEO_STREAM, AUDIO
+    // TODO: Znaleźć możliwe kombinacje headerów dla różnych typów transmisji
+    // TODO: Dopisać typy transmisji
+}
