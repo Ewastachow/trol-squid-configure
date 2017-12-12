f6026234831cf92e5c6ea63385f0466025b6d61d
DESKTOP-3F4BINV\Szymon <Szymek_diks@interia.pl>
2017-12-12 17:11:29 +0100

    Nie bawiłem się jeszcze z userem, ale chętnie to dopiszę, generalnie starałem się zachować nazewnictwo z domainslist

--- src/main/java/trol/domain/trol_api/TrolAPI.java
+++ src/main/java/trol/domain/trol_api/TrolAPI.java
@@ -3,6 +3,9 @@ package trol.domain.trol_api;
 import trol.domain.squid.SquidConf;
 import trol.domain.trol_api.domain.DomainsFile;
 import trol.domain.trol_api.domain.Mode;
+import trol.domain.trol_api.header.Headers;
+import trol.domain.trol_api.header.TransmisionType;
+import trol.domain.trol_api.word.WordsFile;
 
 import java.time.LocalTime;
 import java.util.List;
@@ -59,8 +62,84 @@ public class TrolAPI {
     }
 
 //###################### DOMAINS #############################
+//###################### Headers #############################
 
+    public List<Headers> getHeadersList() {
+        //TODO: Implement
+        return null;
+    }
 
+    public Headers getHeader(TransmisionType mode) {
+        //TODO: Implement
+        return null;
+    }
 
+    public boolean changeHeaderBlockedMode(boolean block) {
+        //TODO: Implement
+        return false;
+    }
+
+    public boolean changeHeaderTime(TransmisionType mode, LocalTime newTimeBegin, LocalTime newTimeEnd) {
+        //TODO: Implement
+        return false;
+    }
+
+    public boolean createNewHeader() {
+        //TODO: Implement
+        return false;
+    }
+
+    public boolean deleteHeader() {
+        //TODO: Implement
+        return false;
+    }
+    //###################### Headers #############################
+    // ###################### Users #############################
+
+    // ###################### Users #############################
+    // ###################### Words #############################
+
+    public List<WordsFile> getWordsFileList(){
+        // Zwraca Listę plików które przechowują blokowane słowa
+        //TODO: Implement
+        return null;
+    }
+
+    public WordsFile getWordsListFromWordsFile(String wordsListName){
+        //TODO: Implement
+        return null;
+    }
+
+    public boolean createNewWordsList(String wordsListName){
+        //TODO: Impement
+        return false;
+    }
+
+    public boolean createWordInList(String wordsListName, String word){
+        //TODO: Implement
+        return false;
+    }
+
+    public boolean deleteWordFromList(String wordsListName, String word){
+        //TODO: Implement
+        return false;
+    }
+
+    public boolean deleteWordsList(String wordsListName){
+        //TODO: Implement
+        return false;
+    }
+
+    public boolean changeBlockModeInWordsList(String wordsListName, boolean block){
+        //TODO: Implement
+        return false;
+    }
+
+    public boolean changeTimeInWordsList(String wordsListName, LocalTime newTimeBegin, LocalTime newTimeEnd){
+        //TODO: Implement
+        return false;
+    }
+
+    // ###################### Words #############################
 
 }
