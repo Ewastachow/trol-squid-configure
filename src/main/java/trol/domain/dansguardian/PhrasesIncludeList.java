2b9e55300cfbfa8ea25547b1c8cb9a7183e6b729
Ewastachow <stachow.ewa@gmail.com>
2017-12-14 21:16:39 +0100

    Skelet for Dansguardian phrases added

---
+++ src/main/java/trol/domain/dansguardian/PhrasesIncludeList.java
@@ -0,0 +1,12 @@
+package trol.domain.dansguardian;
+
+import java.nio.file.Path;
+import java.util.List;
+
+public class PhrasesIncludeList {
+    Path path;
+    List<PhrasesList> contentList;
+
+    //TODO Implement schema for Phrases
+
+}
---
+++ src/main/java/trol/domain/dansguardian/PhrasesList.java
@@ -0,0 +1,14 @@
+package trol.domain.dansguardian;
+
+import java.nio.file.Path;
+import java.util.List;
+
+public class PhrasesList {
+    Path path;
+    List<String> phrasesList;
+
+    public String getConfigString(){
+        //TODO Opakować path znaczkami żeby zrobił inscluda do listy głównej
+        return null;
+    }
+}
