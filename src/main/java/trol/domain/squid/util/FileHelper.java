6c627ce7fa8ff05cb28e076b3c1aeb0bf55fdfd6
Ewastachow <stachow.ewa@gmail.com>
2017-12-28 13:35:33 +0100

    added task

--- src/main/java/trol/domain/squid/util/FileHelper.java
+++ src/main/java/trol/domain/squid/util/FileHelper.java
@@ -28,39 +28,46 @@ public class FileHelper {
         BufferedReader reader = Files.newBufferedReader(filePath, Charset.forName("UTF-8"));
         String line = "";
 
-        while((line = reader.readLine()) != null) {
-            if(line.trim().length() > 0)
+        while ((line = reader.readLine()) != null) {
+            if (line.trim().length() > 0)
                 result.add(line);
         }
 
         return result;
     }
 
-    public static List<String> createWordsListFromLine(String line){
+    public static List<String> createWordsListFromLine(String line) {
         List<String> result = new ArrayList<>();
         result.addAll(Arrays.asList(line.split("[\r \t]+")));
         return result;
     }
 
     /**
-     *
      * @param words
      * @return "" if words is null or words length is equal to zero
      */
-    public static String createStringFromWordsList(List<String> words){
-        if(words == null || words.size() == 0)
+    public static String createStringFromWordsList(List<String> words) {
+        if (words == null || words.size() == 0)
             return "";
 
         StringBuilder builder = new StringBuilder();
-        for(int i = 0; i < words.size()-1; ++i) {
+        for (int i = 0; i < words.size() - 1; ++i) {
             builder.append(words.get(i));
             builder.append(" ");
         }
-        builder.append(words.get(words.size()-1));
+        builder.append(words.get(words.size() - 1));
         return builder.toString();
     }
 
     public static String removeQuotationMarks(String string) {
         return string.replace("\"", "");
+    }
+
+    public static void saveStringAsFile(Path path, String string) {
+        //TODO zakladam ze string zawiera w sb juz np entery
+        //TODO jezeli uwazasz ze zamiast stringa z enterami lepsza by byla lista stringow jako lista linii pliku to zmien
+        //TODO jezeli uwazasz ze jeszcze inna forma bd lepsza to tez zmien
+        //TODO i zapisujemy (nadpisujemy, w pliku ma byc tylko zawartosc stringa) do pliku wskazanego w path, jak nie ma to tworzymy go
+
     }
 }
