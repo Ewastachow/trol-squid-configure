aa6a2e7b11b6876ed9e8416b0b6c7f76c737b789
SzymonMajk <Szymek_diks@interia.pl>
2017-12-30 21:14:05 +0100

    some implementation i logsReader

--- src/main/java/trol/domain/log/BlockTimeManager.java
+++ src/main/java/trol/domain/log/BlockTimeManager.java
@@ -1,21 +1,34 @@
 package trol.domain.log;
 
+import java.time.LocalTime;
 import java.util.List;
 
 public class BlockTimeManager {
     private String timesPath;
     private List<String> users;
+    private LocalTime lastUpdateTimestamp;
 
     BlockTimeManager(String timesPath, List<String> users) {
         this.timesPath = timesPath;
         this.users = users;
+        lastUpdateTimestamp =  LocalTime.now();
     }
+
+    /**
+     * Check if last update was in previous day or not
+     * @return true if now returns date before lastUpdateTimestamp
+     */
+    public boolean nextDay() {
+        return lastUpdateTimestamp.isAfter(LocalTime.now());
+    }
+
 
     public boolean addTime(int seconds, String user) {
         //TODO: ma dopisać do pliku i jeżeli przekroczy użytkownikowi jego blokadę czasową,
         //TODO: to myk myk zwraca true i w ten sposób przekazuje że użytkownika trzeba zablokować
         return false;
     }
+
 
     public void clearWastedTime() {
         //TODO: Wywoływana domyślnie o 00:00 chodzi o wyzerowanie wasted timeu w pliku
--- src/main/java/trol/domain/log/LogsReader.java
+++ src/main/java/trol/domain/log/LogsReader.java
@@ -1,6 +1,8 @@
 package trol.domain.log;
 
 import trol.domain.squid.util.FileHelper;
+import trol.domain.terminal.TerminalExecute;
+
 import java.io.IOException;
 import java.net.Inet4Address;
 import java.net.InetAddress;
@@ -13,17 +15,19 @@ import java.util.Set;
 public class LogsReader {
 
     private BlockTimeManager timeManager;
+    private TerminalExecute term;
     private String accessLogPath;
     private int lastLine;
     private List<String> users;
     private Set<String> foundUsers = new HashSet<>();
     private Set<InetAddress> usersReadyToBlock = new HashSet<>();
 
-    LogsReader(String accessLogPath, List<Inet4Address> users) {
-        this(accessLogPath,users,"/etc/squid/blockTimes");
+    LogsReader(String accessLogPath, List<Inet4Address> users, TerminalExecute term) {
+        this(accessLogPath,users, term, "/etc/squid/blockTimes");
     }
 
-    LogsReader(String accessLogPath, List<Inet4Address> users, String blockTimesPath) {
+    LogsReader(String accessLogPath, List<Inet4Address> users, TerminalExecute term, String blockTimesPath) {
+        this.term = term;
         this.accessLogPath = accessLogPath;
         for(Inet4Address l : users) {
             this.users.add(l.getHostAddress());
@@ -31,15 +35,22 @@ public class LogsReader {
         timeManager = new BlockTimeManager(blockTimesPath,this.users);
         lastLine = 0;
     }
+
     /**
-     * Should be called in equal intervals close to one minute to obtain good performance.
+     * Should be called in equal intervals by other thread
      * @return List of Inet4Addresses of users that are ready to block or empty list.
      * @throws UnknownHostException When users list is invalid.
      * @throws IOException problems with logs or block times file
      */
-    public List<InetAddress> checkUsersLogs() throws IOException {
-        updateWastedTime();
+    public List<InetAddress> checkUsersLogs() throws IOException, InterruptedException {
         ArrayList<InetAddress> result = new ArrayList<>();
+
+        if(timeManager.nextDay()) {
+            updateNextDay();
+        }
+        else {
+            updateUsersReadyToBlock();
+        }
 
         for(InetAddress u : usersReadyToBlock)
             result.add(u);
@@ -47,20 +58,23 @@ public class LogsReader {
         return result;
     }
 
-    public void reset() {
-        lastLine = 0;
+    private void updateNextDay() throws IOException, InterruptedException {
         timeManager.clearWastedTime();
+        usersReadyToBlock.clear();
+        lastLine = 0;
+        term.executeCommand("squid -k rotate");
     }
 
-    private void updateWastedTime() throws IOException {
-        List<String> newLines = FileHelper.readLastLinesSince(accessLogPath,lastLine);
+    private void updateUsersReadyToBlock() throws IOException {
+
+        List<String> newLines = FileHelper.readLastLinesSince(accessLogPath, lastLine);
         lastLine += newLines.size();
 
-        for(String l : newLines)
+        for (String l : newLines)
             parseLine(l);
 
-        for(String user : foundUsers) {
-            if (timeManager.addTime(60,user))
+        for (String user : foundUsers) {
+            if (timeManager.addTime(60, user))
                 usersReadyToBlock.add(Inet4Address.getByName(user));
         }
 
